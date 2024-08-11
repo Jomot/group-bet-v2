92bfec56ffe9f9a2d75cecb613e565c8b8b63aee
Tom <tomjones3198@gmail.com>
2024-08-11 13:06:02 +0100

    initial commit

---
+++ src/Test.ts
@@ -0,0 +1,22 @@
+import fetch, { Request, Response } from 'node-fetch';
+
+// const headers = new Headers();
+// headers.append('x-rapidapi-key', '144493da2amsh75503a655f98956p188a2ejsnb67f35b12c2c');
+// headers.append('x-rapidapi-host', 'v3.football.api-sports.io');
+
+// var requestOptions = {
+//   method: 'GET',
+//   headers: headers,
+//   redirect: 'follow',
+// };
+
+// const request = new Request('https://v3.football.api-sports.io/leagues', {
+//   method: 'GET',
+//   headers: headers,
+//   redirect: 'follow',
+// });
+
+// const response = await fetch(request);
+// console.log(response);
+
+console.log(`Hello World`);
